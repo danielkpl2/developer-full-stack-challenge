fc0d3e7194d40bb53c141444fd9412c1b9c4841d
Daniel Kasprowicz <daniel@Daniels-MacBook-Pro.local>
2023-10-05 23:30:03 +0100

    set up the prod and dev environments

--- src/api/main.py
+++ src/api/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from starlette.responses import RedirectResponse
+import os
+ENVIRONMENT = os.getenv("ENVIRONMENT", "dev")
+DEV_URL = os.getenv("DEV_URL", "http://localhost:3000/")
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# if in production then serve the nuxt generated html file (presumed it exists)
+if ENVIRONMENT == "prod":
+    app.mount("/", StaticFiles(directory="../dashboard/dist", html=True), name="static")
+# if in dev then redirect to the dev server
+elif ENVIRONMENT == "dev":
+    @app.get("/")
+    def read_root():
+        return RedirectResponse(DEV_URL)
